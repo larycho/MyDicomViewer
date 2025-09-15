66d6172c5cb8afda61fb81adda722963887cfe05
KarolinaMoz <sample@users.noreply.com>
2025-09-15 19:02:17 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.mydicomviewer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.mydicomviewer to javafx.fxml;
+    exports org.example.mydicomviewer;
+}
